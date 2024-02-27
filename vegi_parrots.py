e37d8c51a142bece13eb6faf1097856193916def
“petitmarronn” <ichiko28@gmail.com>
2024-02-27 12:24:37 +0900

    first commit

---
+++ vegi_parrots.py
@@ -0,0 +1,60 @@
+import os
+from flask import Flask, request, redirect, render_template, flash
+from werkzeug.utils import secure_filename
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.preprocessing import image
+
+import numpy as np
+
+
+classes = ["小松菜","豆苗","にんじん","ピーマン","アボカド","玉ねぎ","にら"]
+image_size = 50
+
+UPLOAD_FOLDER = "up"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+model = load_model('./model.h5')#学習済みモデルをロード
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('ファイルがありません')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('ファイルがありません')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            filepath = os.path.join(UPLOAD_FOLDER, filename)
+
+            #受け取った画像を読み込み、np形式に変換
+            img = image.load_img(filepath, target_size=(image_size,image_size))
+            img = image.img_to_array(img)
+            data = np.array([img])
+            #変換したデータをモデルに渡して予測する
+            predictions = model.predict(data)[0]
+            predicted_class_index = predictions.argmax()
+            predicted_class = classes[predicted_class_index]
+
+            if predicted_class in ["アボカド", "玉ねぎ", "にら"]:
+                 pred_answer = "これは " + predicted_class + " です。絶対にあげないでください。"
+            else:
+                 pred_answer = "これは " + predicted_class + " です。副菜としてあげられます。"
+
+            return render_template("index.html",answer=pred_answer)
+
+    return render_template("index.html",answer="")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host ='0.0.0.0',port = port)
